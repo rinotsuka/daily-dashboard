14150497a18e26b710d0a9405624a55c73429b02
tsuka-rinorino <info@sora-maru.io>
2024-06-23 17:38:20 +0900

    settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,37 +1,64 @@
 module.exports = {
-  extends: 'erb',
-  plugins: ['@typescript-eslint'],
-  rules: {
-    // A temporary hack related to IDE not resolving correct package.json
-    'import/no-extraneous-dependencies': 'off',
-    'react/react-in-jsx-scope': 'off',
-    'react/jsx-filename-extension': 'off',
-    'import/extensions': 'off',
-    'import/no-unresolved': 'off',
-    'import/no-import-module-exports': 'off',
-    'no-shadow': 'off',
-    '@typescript-eslint/no-shadow': 'error',
-    'no-unused-vars': 'off',
-    '@typescript-eslint/no-unused-vars': 'error',
-  },
+  extends: ["erb", "prettier"],
+  plugins: ["react", "@typescript-eslint", "prettier", "import", "react-hooks"],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: 2022,
-    sourceType: 'module',
+    sourceType: "module",
+  },
+  rules: {
+    curly: "error",
+    "no-var": "error",
+    strict: ["error", "never"],
+    eqeqeq: ["error", "always"],
+    "no-console": ["error", { allow: ["error"] }],
+    "spaced-comment": ["error", "always"],
+    "import/no-duplicates": "error",
+    "dot-notation": "error",
+    "no-else-return": "error",
+    "no-eval": "error",
+    "no-fallthrough": "error",
+    "no-implicit-coercion": "error",
+    "no-loop-func": "error",
+    "no-restricted-imports": ["error", { patterns: ["./", "../"] }],
+    "no-unused-vars": [
+      "error",
+      {
+        argsIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+        caughtErrorsIgnorePattern: "^_",
+        destructuredArrayIgnorePattern: "^_",
+      },
+    ],
+    "@typescript-eslint/no-explicit-any": "error",
+    "@typescript-eslint/array-type": "error",
+    "@typescript-eslint/adjacent-overload-signatures": "error",
+    "react/self-closing-comp": "error",
+    "react/jsx-curly-brace-presence": ["error", { props: "never", children: "never" }],
+    "react-hooks/exhaustive-deps": "error",
+    "import/order": [
+      "error",
+      {
+        groups: ["builtin", "external", "internal", "parent", "sibling", "index"],
+        "newlines-between": "never",
+        alphabetize: { order: "asc", caseInsensitive: true },
+      },
+    ],
+    "prettier/prettier": "error",
   },
   settings: {
-    'import/resolver': {
-      // See https://github.com/benmosher/eslint-plugin-import/issues/1396#issuecomment-575727774 for line below
+    "import/resolver": {
       node: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
-        moduleDirectory: ['node_modules', 'src/'],
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+        moduleDirectory: ["node_modules", "src/"],
       },
       webpack: {
-        config: require.resolve('./.erb/configs/webpack.config.eslint.ts'),
+        config: require.resolve("./.erb/configs/webpack.config.eslint.ts"),
       },
       typescript: {},
     },
-    'import/parsers': {
-      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"],
     },
   },
-};
+}
--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,6 +1,6 @@
-import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
-import icon from '../../assets/icon.svg';
-import './App.css';
+import { MemoryRouter as Router, Routes, Route } from "react-router-dom"
+import icon from "../../assets/icon.svg"
+import "./App.css"
 
 function Hello() {
   return (
@@ -10,11 +10,7 @@ function Hello() {
       </div>
       <h1>electron-react-boilerplate</h1>
       <div className="Hello">
-        <a
-          href="https://electron-react-boilerplate.js.org/"
-          target="_blank"
-          rel="noreferrer"
-        >
+        <a href="https://electron-react-boilerplate.js.org/" target="_blank" rel="noreferrer">
           <button type="button">
             <span role="img" aria-label="books">
               📚
@@ -22,11 +18,7 @@ function Hello() {
             Read our docs
           </button>
         </a>
-        <a
-          href="https://github.com/sponsors/electron-react-boilerplate"
-          target="_blank"
-          rel="noreferrer"
-        >
+        <a href="https://github.com/sponsors/electron-react-boilerplate" target="_blank" rel="noreferrer">
           <button type="button">
             <span role="img" aria-label="folded hands">
               🙏
@@ -36,7 +28,7 @@ function Hello() {
         </a>
       </div>
     </div>
-  );
+  )
 }
 
 export default function App() {
@@ -46,5 +38,5 @@ export default function App() {
         <Route path="/" element={<Hello />} />
       </Routes>
     </Router>
-  );
+  )
 }
