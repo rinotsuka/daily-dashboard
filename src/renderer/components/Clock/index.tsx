9c1d3e9711cd2a1fcb914e2dd8a4edb6cdd22f6b
tsuka-rinorino <info@sora-maru.io>
2024-06-25 21:11:02 +0900

    時計作成

--- src/renderer/components/Clock/index.tsx
+++ src/renderer/components/Clock/index.tsx
@@ -1,8 +1,25 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react"
+import dayjs from "dayjs"
+
+// 現在の日付と時刻を取得
+const now = dayjs()
+const year = now.format("YYYY")
+const month = now.format("MM")
+const day = now.format("DD")
+const hour = now.format("HH")
+const minute = now.format("mm")
+
+const reiwaYear = "令和" + (Number(year) - 2018)
 
 export default function Clock() {
-  return <div css={styles.root}>時計</div>
+  return (
+    <div css={styles.root}>
+      {`${reiwaYear}年`}
+      {`${year}年${month}月${day}日`}
+      {`${hour}:${minute}`}
+    </div>
+  )
 }
 
 const styles = {
