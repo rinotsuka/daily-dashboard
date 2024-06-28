21ae2742b05594579d6fec1534c52c9826890a95
tsuka-rinorino <katsunori@payapaya.dev>
2024-06-28 15:39:00 +0900

    サイズ 変更

--- src/main/main.ts
+++ src/main/main.ts
@@ -71,7 +71,7 @@ const createWindow = async () => {
   mainWindow = new BrowserWindow({
     show: false,
     width: 1024,
-    height: 728,
+    height: 600,
     icon: getAssetPath("icon.png"),
     webPreferences: {
       preload: app.isPackaged ? path.join(__dirname, "preload.js") : path.join(__dirname, "../../.erb/dll/preload.js"),
